adf5bd1645b1cf049a92fe3ce668fa9eecafaaa1
Sean Farr <seantf68@gmail.com>
2021-11-20 15:59:19 -0500

    modifying line colors and adding destinations to lines in logic.js

--- Mapping_GoeJSON_Linestrings/static/js/logic.js
+++ Mapping_GoeJSON_Linestrings/static/js/logic.js
@@ -1,48 +1,69 @@
-// // Add console.log to check to see if our code is working.
-// console.log("working");
-// // Create the map object with a center at the San Francisco airport and zoom level.
-// let map = L.map("mapid").setView([30, 30], 2);
-
 // We create the streets tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let day = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
-  Dark: dark
+  'Day Navigation': day,
+  'Night Navigation': night
 };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
-  zoom: 2,
-  layers: [streets]
+  center: [44.0, -80.0],
+  zoom: 3,
+  layers: [day]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/SeanTFarr/Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/SeanTFarr/Mapping_Earthquakes/main/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
   
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    style: myStyle,
     // We turn each feature into a marker on the map(using onEachFeature).
-    onEachFeature: function(feature, layer) {
+    onEachFeature: function(feature ,layer) {
       console.log(layer);
-      
-      layer.bindPopup("<h2> Airport Code:" + layer.feature.properties.faa + "</h2> <hr> <h3> Airport Name: " + layer.feature.properties.name);
+      layer.bindPopup("<h2> Airline:" + layer.feature.properties.airline + "</h2> <hr> <h3> Destination: " + layer.feature.properties.dst);
     }
-  }).addTo(map);
+  }
+  ).addTo(map);
 });
+
+// // Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/SeanTFarr/Mapping_Earthquakes/main/majorAirports.json";
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//   console.log(data);
+  
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJSON(data, {
+//     // We turn each feature into a marker on the map(using onEachFeature).
+//     onEachFeature: function(feature, layer) {
+//       console.log(layer);
+      
+//       layer.bindPopup("<h2> Airport Code:" + layer.feature.properties.faa + "</h2> <hr> <h3> Airport Name: " + layer.feature.properties.name);
+//     }
+//   }).addTo(map);
+// });
+
+// https://raw.githubusercontent.com/SeanTFarr/Mapping_Earthquakes/main/torontoRoutes.json
