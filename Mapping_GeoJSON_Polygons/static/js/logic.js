934ea795feed7283cd207f32fa474f1a12ce88a2
Sean Farr <seantf68@gmail.com>
2021-11-21 16:33:01 -0500

    adding copied files from Mapping_GeoJSON_Linestrings branch and modifying logic.js file

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,69 +1,49 @@
 // We create the streets tile layer that will be the background of our map.
-let day = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets= L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // We create the dark view tile layer that will be an option for our map.
-let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // Create a base layer that holds both maps.
 let baseMaps = {
-  'Day Navigation': day,
-  'Night Navigation': night
+  'Streets': streets,
+  'Satellite Streets': satelliteStreets
 };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [44.0, -80.0],
-  zoom: 3,
-  layers: [day]
+  center: [43.7, -79.3],
+  zoom: 10,
+  layers: [streets]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/SeanTFarr/Mapping_Earthquakes/main/torontoRoutes.json";
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = 'https://raw.githubusercontent.com/SeanTFarr/Mapping_Earthquakes/main/torontoNeighborhoods.json'
 
-// Create a style for the lines.
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  color: 'blue',
+  fillColor: 'yellow',
+  weight: 1
 }
-// Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-  console.log(data);
-  
-  // Creating a GeoJSON layer with the retrieved data.
+
+// Grabbing the GeoJSON data
+d3.json(torontoHoods).then(function(data){
+  console.log(data, );
+  // Create a GeoJSON layer with the retrieved data
   L.geoJSON(data, {
     style: myStyle,
     // We turn each feature into a marker on the map(using onEachFeature).
     onEachFeature: function(feature ,layer) {
       console.log(layer);
-      layer.bindPopup("<h2> Airline:" + layer.feature.properties.airline + "</h2> <hr> <h3> Destination: " + layer.feature.properties.dst);
+      layer.bindPopup("<h2> Neighborhood:" + layer.feature.properties.AREA_NAME + "</h2>");
     }
-  }
-  ).addTo(map);
-});
+  }).addTo(map);
+})
 
-// // Accessing the airport GeoJSON URL
-// let airportData = "https://raw.githubusercontent.com/SeanTFarr/Mapping_Earthquakes/main/majorAirports.json";
-
-// // Grabbing our GeoJSON data.
-// d3.json(airportData).then(function(data) {
-//   console.log(data);
-  
-// // Creating a GeoJSON layer with the retrieved data.
-// L.geoJSON(data, {
-//     // We turn each feature into a marker on the map(using onEachFeature).
-//     onEachFeature: function(feature, layer) {
-//       console.log(layer);
-      
-//       layer.bindPopup("<h2> Airport Code:" + layer.feature.properties.faa + "</h2> <hr> <h3> Airport Name: " + layer.feature.properties.name);
-//     }
-//   }).addTo(map);
-// });
-
-// https://raw.githubusercontent.com/SeanTFarr/Mapping_Earthquakes/main/torontoRoutes.json
